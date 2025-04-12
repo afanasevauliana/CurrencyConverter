c35ff1c2fafdd31a7b25899716eff0d29546e331
afanasevauliana <afanasevauliana946@gmail.com>
2025-04-12 15:00:26 +0300

    project start

--- build-CurrencyConverter-Desktop_Qt_5_12_12_MinGW_32_bit-Debug/ui_mainwindow.h
+++ build-CurrencyConverter-Desktop_Qt_5_12_12_MinGW_32_bit-Debug/ui_mainwindow.h
@@ -11,6 +11,7 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
@@ -22,6 +23,8 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QComboBox *comboBox;
+    QComboBox *comboBox_2;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -29,12 +32,27 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(800, 600);
+        MainWindow->resize(472, 312);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        comboBox = new QComboBox(centralwidget);
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+        comboBox->setGeometry(QRect(120, 30, 221, 41));
+        comboBox->setStyleSheet(QString::fromUtf8("font: 63 25pt \"Yu Gothic UI Semibold\";"));
+        comboBox_2 = new QComboBox(centralwidget);
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->setObjectName(QString::fromUtf8("comboBox_2"));
+        comboBox_2->setGeometry(QRect(120, 150, 221, 41));
+        comboBox_2->setStyleSheet(QString::fromUtf8("font: 63 25pt \"Yu Gothic UI Semibold\";"));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 472, 26));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -48,6 +66,14 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        comboBox->setItemText(0, QApplication::translate("MainWindow", "QLineEdit", nullptr));
+        comboBox->setItemText(1, QApplication::translate("MainWindow", "QPushButton", nullptr));
+        comboBox->setItemText(2, QApplication::translate("MainWindow", "QLabel", nullptr));
+
+        comboBox_2->setItemText(0, QApplication::translate("MainWindow", "QLineEdit", nullptr));
+        comboBox_2->setItemText(1, QApplication::translate("MainWindow", "QPushButton", nullptr));
+        comboBox_2->setItemText(2, QApplication::translate("MainWindow", "QLabel", nullptr));
+
     } // retranslateUi
 
 };
